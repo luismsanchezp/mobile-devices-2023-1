a20a2ffe3df8c1346415dd1e8927633389f368ca
Luis Miguel Sanchez Pinilla <luism.sanchezp@UAM.LOCAL>
2023-01-24 19:47:01 -0500

    Implement methods for PUT and DELETE in user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -32,8 +32,8 @@ USER_ROUTES.get("/", (req, res) => {
 });
 
 USER_ROUTES.get("/:user_id", (req, res) => {
-    user_id = req.params.user_id;
-    USER_SCHEMA.findById({_id: user_id})
+    const user_id = req.params.user_id;
+    USER_SCHEMA.findById(user_id)
         .then(
             (data) => res.json(data)
         )
@@ -46,7 +46,29 @@ USER_ROUTES.get("/:user_id", (req, res) => {
         );
 });
 
-USER_ROUTES.put("/:user_id", (req, res) => {});
-USER_ROUTES.delete("/:user_id", (req, res) => {});
+USER_ROUTES.put("/:user_id", (req, res) => {
+    const user_id = req.params.user_id;
+    const { user_name, age, email, address } = req.body;
+    
+    var dict = {};
+    if (user_name !== undefined) dict.user_name = user_name;
+    if (age !== undefined) dict.age = age;
+    if (email !== undefined) dict.email = email;
+    if (address !== undefined) dict.address = address;
+
+    USER_SCHEMA.updateOne(
+        { _id : user_id },
+        dict
+    )
+    .then((data) => res.json(data))
+    .catch((err) => res.json({ message: err }));
+});
+
+USER_ROUTES.delete("/:user_id", (req, res) => {
+    const user_id = req.params.user_id;
+    USER_SCHEMA.findOneAndRemove(user_id)
+    .then((data) => res.json(data))
+    .catch((err) => res.json({ message: err}));
+});
 
 module.exports = USER_ROUTES;
